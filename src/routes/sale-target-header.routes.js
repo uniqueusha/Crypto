e770c11844b83be7037f47abc8c8a80376da0be0
uniqueusha <ushamyadav777@gmail.com>
2024-12-20 15:20:02 +0530

    get set target list

--- src/routes/sale-target-header.routes.js
+++ src/routes/sale-target-header.routes.js
@@ -6,6 +6,7 @@ const checkAuth = require("../middleware/check.auth");
 //add sale target header
 router.post('/', checkAuth, saleTargetHeaderController.addSaleTargetHeader)
 router.post('/currant-price', saleTargetHeaderController.createCurrentPrice);
+router.get('/', saleTargetHeaderController.getSetTargets);
 router.put('/', checkAuth, saleTargetHeaderController.currantPriceUpdateTargetComplitionStatus);
 
 module.exports = router
